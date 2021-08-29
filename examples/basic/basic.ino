a799c337fa1834c0b970be646ae012100f503a6a
Mindaugas Žvirblis <mz@9g.lt>
2021-08-29 14:18:17 +0300

    renaming things for arduino library manager

---
+++ examples/basic/basic.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#include <MCP23017.h>
+
+MCP23017 mcp;
+
+void setup()
+{
+
+  mcp.begin();
+  for (int i = 0; i < 16; i++)
+  {
+    mcp.pinMode(i, MCP_OUTPUT);
+  }
+  pinMode(3, OUTPUT);
+
+  mcp.pinMode(15, MCP_PULLUP);
+}
+
+void loop()
+{
+
+  mcp.digitalWrite(1, mcp.digitalRead(15));
+
+  mcp.digitalWrite(8, LOW); // PORT B pin 0
+  mcp.digitalWrite(0, HIGH); // PORT A pin 0
+  
+  delay(100);
+  mcp.digitalWrite(8, HIGH); // PORT B pin 0
+  mcp.digitalWrite(0, LOW); // PORT A pin 0
+  digitalWrite(3, LOW);
+  delay(100);
+}
